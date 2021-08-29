a4aaa2b07fb3a70ff8f8af33a4d5bac9f811c2cc
Mollen <necr94@gmail.com>
2021-08-29 23:13:19 +0300

    ru language done

--- source/sort.js
+++ source/sort.js
@@ -6,12 +6,12 @@ let sort = unSortedString => {
     sortedString.forEach( (word, wordPosition) => {
         word = word.toLowerCase();
         let symbolsInWord = word.split('');
-        symbolsInWord.sort();
+        symbolsInWord.sort( (a, b) => Intl.Collator('ru').compare(a, b) );
         symbolsInWord[0] = symbolsInWord[0].toUpperCase();
         sortedString[wordPosition] = symbolsInWord.join('');
     });
 
-    sortedString.sort();
+    sortedString.sort( (a, b) => Intl.Collator('ru').compare(a, b) );
 
     return sortedString.join(' ')
 }
