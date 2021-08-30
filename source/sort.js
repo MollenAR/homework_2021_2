af9b6b7bb9404c8f1ab438bedaec545013837f08
Mollen <necr94@gmail.com>
2021-08-30 03:07:38 +0300

    less text and fixes

--- source/sort.js
+++ source/sort.js
@@ -6,18 +6,30 @@
 
 'use strict';
 
-let sort = unSortedString => {
-    let sortedString = unSortedString.split(' ');
+// let sort = unSortedString => {
+//     let sortedString = unSortedString.split(' ');
 
-    sortedString.forEach( (word, wordPosition) => {
-        word = word.toLowerCase();
-        let symbolsInWord = word.split('');
-        symbolsInWord.sort( (a, b) => Intl.Collator(['ru', 'jp']).compare(a, b) );
-        symbolsInWord[0] = symbolsInWord[0].toUpperCase();
-        sortedString[wordPosition] = symbolsInWord.join('');
+//     sortedString.forEach( (word, wordPosition) => {
+//         word = word.toLowerCase();
+//         let symbolsInWord = word.split('');
+//         symbolsInWord.sort( (a, b) => Intl.Collator(['ru', 'jp']).compare(a, b) );
+//         symbolsInWord[0] = symbolsInWord[0].toUpperCase();
+//         sortedString[wordPosition] = symbolsInWord.join('');
+//     });
+
+//     sortedString.sort( (a, b) => Intl.Collator(['ru', 'jp']).compare(a, b) );
+
+//     return sortedString.join(' ')
+// }
+
+let sort = unSortedString => {
+    let sortedString = '';
+
+    unSortedString.split(' ').forEach(word => {
+        word = word.toLowerCase().split('').sort( (a, b) => new Intl.Collator(['ru', 'jp']).compare(a, b) );
+        word[0] = word[0].toUpperCase();
+        sortedString += word.join('') + ' ';
     });
 
-    sortedString.sort( (a, b) => Intl.Collator(['ru', 'jp']).compare(a, b) );
-
-    return sortedString.join(' ')
+    return sortedString.split(' ').sort( (a, b) => new Intl.Collator(['ru', 'jp']).compare(a, b) ).join(' ').trim()
 }
