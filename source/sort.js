a6c162cc60f1ef27dd708b94a7cd79849185d62c
Mollen <necr94@gmail.com>
2021-08-30 03:09:17 +0300

    deleted comments

--- source/sort.js
+++ source/sort.js
@@ -6,22 +6,6 @@
 
 'use strict';
 
-// let sort = unSortedString => {
-//     let sortedString = unSortedString.split(' ');
-
-//     sortedString.forEach( (word, wordPosition) => {
-//         word = word.toLowerCase();
-//         let symbolsInWord = word.split('');
-//         symbolsInWord.sort( (a, b) => Intl.Collator(['ru', 'jp']).compare(a, b) );
-//         symbolsInWord[0] = symbolsInWord[0].toUpperCase();
-//         sortedString[wordPosition] = symbolsInWord.join('');
-//     });
-
-//     sortedString.sort( (a, b) => Intl.Collator(['ru', 'jp']).compare(a, b) );
-
-//     return sortedString.join(' ')
-// }
-
 let sort = unSortedString => {
     let sortedString = '';
 
